c967482661e63dcd985e1a132729b2245f92a3ad
Benny Lim <bennylim0926@gmail.com>
2023-04-25 23:13:53 +0800

    Setup Project

---
+++ app/src/main/java/com/sns/sns_nativeapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.sns.sns_nativeapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
